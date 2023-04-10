7dc95cf2b42ada36e185637a99475a1e53a615a7
chalie2123@gmail.com <chalie2123@gmail.com>
2023-04-10 11:04:13 +0300

    some frot end css change and componet update

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,31 +1,44 @@
-import React, { useEffect, useState } from "react";
-import Edit from "../img/edit.png";
-import Delete from "../img/delete.png";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import Menu from "../components/Menu";
-import axios from "axios";
-import moment from "moment";
-import { useContext } from "react"; 
-import DOMPurify from "dompurify";
+import React, { useEffect, useState } from 'react';
+import Edit from '../img/edit.png';
+import Delete from '../img/delete.png';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Menu from '../components/Menu';
+import axios from 'axios';
+import moment from 'moment';
+import { useContext } from 'react';
+import DOMPurify from 'dompurify';
 
 const Single = () => {
-  
- 
-
   return (
-    <div className="single">
-      <div className="content">
-        <img src="" alt="" />
-        <div className="user">
-          
-          <div className="info">
-            <span></span>
-            <p>Posted</p>
+    <div className='single'>
+      <div className='content'>
+        <img
+          src='https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          alt=''
+        />
+        <div className='user'>
+          <div className='info'>
+            <span>Chalie</span>
+            <p>Posted 2 day ago</p>
           </div>
-          
+
+          <div className='edit'>
+            <Link to={`write?edit=2`}>
+              <img src={Edit}></img>
+            </Link>
+            <img src={Delete}></img>
+          </div>
         </div>
         <h1>this is para</h1>
-        <p ></p>      </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          officiis sint officia suscipit ratione rerum voluptate iusto non sit
+          tempora minima, quisquam quod ea sed consequatur sequi. Ea, dicta
+          quam!
+        </p>
+      </div>
+
+      <Menu />
     </div>
   );
 };
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,32 +1,21 @@
-import React, { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import axios from "axios";
- 
-const Write = () => {
- 
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import axios from 'axios';
 
- 
-  
+const Write = () => {
+  const [value, setValue] = useState('');
+
   return (
-    <div className="add">
-      <div className="content">
-        <input
-          type="text"
-          placeholder="Title"
-           
-        />
-        <div className="editorContainer">
-          <ReactQuill
-            className="editor"
-            theme="snow"
-            value=""
-            
-          />
+    <div className='add'>
+      <div className='content'>
+        <input type='text' placeholder='Title' />
+        <div className='editorContainer'>
+          <ReactQuill className='editor' theme='snow' value='' />
         </div>
       </div>
-      <div className="menu">
-        <div className="item">
+      <div className='menu'>
+        <div className='item'>
           <h1>Publish</h1>
           <span>
             <b>Status: </b> Draft
@@ -34,88 +23,40 @@ const Write = () => {
           <span>
             <b>Visibility: </b> Public
           </span>
-          <input
-            style={{ display: "none" }}
-            type="file"
-            id="file"
-            name=""
-            
-          />
-          <label className="file" htmlFor="file">
+          <input style={{ display: 'none' }} type='file' id='file' name='' />
+          <label className='file' htmlFor='file'>
             Upload Image
           </label>
-          <div className="buttons">
+          <div className='buttons'>
             <button>Save as a draft</button>
-            <button  >Publish</button>
+            <button>Publish</button>
           </div>
         </div>
-        <div className="item">
+        <div className='item'>
           <h1>Category</h1>
-          <div className="cat">
-            <input
-              type="radio"
-               
-              name="cat"
-              value="art"
-              id="art"
-              
-            />
-            <label htmlFor="art">Art</label>
+          <div className='cat'>
+            <input type='radio' name='cat' value='art' id='art' />
+            <label htmlFor='art'>Art</label>
           </div>
-          <div className="cat">
-            <input
-              type="radio"
-               
-              name="cat"
-              value="science"
-              id="science"
-              
-            />
-            <label htmlFor="science">Science</label>
+          <div className='cat'>
+            <input type='radio' name='cat' value='science' id='science' />
+            <label htmlFor='science'>Science</label>
           </div>
-          <div className="cat">
-            <input
-              type="radio"
-             
-              name="cat"
-              value="technology"
-              id="technology"
-              
-            />
-            <label htmlFor="technology">Technology</label>
+          <div className='cat'>
+            <input type='radio' name='cat' value='technology' id='technology' />
+            <label htmlFor='technology'>Technology</label>
           </div>
-          <div className="cat">
-            <input
-              type="radio"
-           
-              name="cat"
-              value="cinema"
-              id="cinema"
-             
-            />
-            <label htmlFor="cinema">Cinema</label>
+          <div className='cat'>
+            <input type='radio' name='cat' value='cinema' id='cinema' />
+            <label htmlFor='cinema'>Cinema</label>
           </div>
-          <div className="cat">
-            <input
-              type="radio"
-           
-              name="cat"
-              value="design"
-              id="design"
-              
-            />
-            <label htmlFor="design">Design</label>
+          <div className='cat'>
+            <input type='radio' name='cat' value='design' id='design' />
+            <label htmlFor='design'>Design</label>
           </div>
-          <div className="cat">
-            <input
-              type="radio"
-             
-              name="cat"
-              value="food"
-              id="food"
-            
-            />
-            <label htmlFor="food">Food</label>
+          <div className='cat'>
+            <input type='radio' name='cat' value='food' id='food' />
+            <label htmlFor='food'>Food</label>
           </div>
         </div>
       </div>
